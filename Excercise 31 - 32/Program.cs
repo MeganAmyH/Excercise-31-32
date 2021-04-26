971368169826ffce0ace5ee50fe82d6563a40a26
meganamyh <meganamyh@gmail.com>
2021-04-26 18:30:28 -0400

    First Commit

---
+++ Excercise 31 - 32/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Excercise_31___32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 2, 8, 0, 24, 51 };
+            {
+                Console.WriteLine("Please enter an index number, 0-4. ");
+                int input = int.Parse(Console.ReadLine());
+                Console.WriteLine($"The number at index {input} is {nums}");
+            }
+        }
+
+
+    }
+}
